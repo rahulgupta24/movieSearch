af122fce1360f35cdddf04c061acad48f7029aee
rahulgupta24 <rahulgupta24dec@gmail.com>
2023-08-12 17:56:02 +0530

    Movie search code

---
+++ README.md
@@ -0,0 +1,43 @@
+# Movie Search App
+
+The Movie Search App is a web application that allows users to search for movies, view their details, and manage a list of favorite movies.
+
+## Features
+
+- Search for movies by title using the search input.
+- Display search results as movie cards with titles, posters, and options to add to favorites or view details.
+- View movie details, including title, poster, year, rating, plot, etc., on a dedicated page.
+- Add and remove movies to/from the list of favorite movies.
+- View the list of favorite movies on a separate "My Favorites" page.
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Bootstrap CSS for responsive design
+- The Open Movie Database (OMDb) API for movie data
+
+## File Structure
+
+- `index.html`: Main page for searching movies.
+- `my-favorites.html`: Page to display favorite movies.
+- `movie-details.html`: Page to display detailed information about a movie.
+- `styles.css`: Stylesheet for the app's visual appearance.
+- `scripts.js`: JavaScript file containing the app's functionality.
+- `README.md`: This readme file providing an overview of the app.
+
+## Usage
+
+1. Open `index.html` in a web browser to search for movies.
+2. Enter a movie title in the search input and click "Search".
+3. Browse through the search results and click "View Details" to see more about a movie.
+4. On the movie details page, you can also add the movie to your favorites.
+5. Click "My Favorites" to view your list of favorite movies on the `my-favorites.html` page.
+6. On the "My Favorites" page, you can remove movies from your favorites list.
+
+## Acknowledgments
+
+This app was created as a practice project and utilizes the OMDb API for movie data.
+
+
---
+++ scripts.js
@@ -0,0 +1,184 @@
+// scripts.js
+
+// Define apiKey
+const apiKey = '7b75ecdd';
+
+// DOM elements
+const searchInput = document.getElementById('searchInput');
+const searchButton = document.getElementById('searchButton');
+const viewFavoritesButton = document.getElementById('viewFavoritesButton');
+const mainContent = document.getElementById('mainContent');
+const searchResults = document.getElementById('searchResults');
+const favoritesList = document.getElementById('favoritesList');
+const detailsContainer = document.getElementById('movieDetailsContainer');
+
+// Add event listeners
+if (searchButton) {
+    searchButton.addEventListener('click', searchMovies);
+}
+
+if (viewFavoritesButton) {
+    viewFavoritesButton.addEventListener('click', () => {
+        window.location.href = 'my-favorites.html';
+    });
+}
+
+// Add event listener on my-favorites.html
+if (window.location.pathname === '/my-favorites.html') {
+    document.addEventListener('DOMContentLoaded', () => {
+        const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+        displayFavorites(favorites);
+    });
+}
+
+let searchTimeout;
+
+// Function to search movies
+async function searchMovies() {
+    const searchTerm = searchInput.value;
+
+    // Clear the previous timeout to prevent unnecessary requests
+    clearTimeout(searchTimeout);
+
+    // Delay the search by a short duration to wait for user input
+    searchTimeout = setTimeout(async () => {
+        const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&s=${searchTerm}`);
+        const data = await response.json();
+
+        mainContent.innerHTML = '';
+
+        if (data.Search) {
+            data.Search.forEach(movie => {
+                const movieElement = createMovieElement(movie, true);
+                mainContent.appendChild(movieElement);
+            });
+        }
+    }, 300); // Adjust the delay duration as needed
+}
+
+// Add event listener for keyup and Enter key press
+searchInput.addEventListener('keyup', searchMovies);
+
+// Listen for Enter key press and trigger search
+searchInput.addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault(); // Prevent form submission
+        searchMovies();
+    }
+});
+
+// Function to create a movie element
+function createMovieElement(movie, isSearchResult) {
+    const movieElement = document.createElement('div');
+    movieElement.classList.add('movie');
+    movieElement.innerHTML = `
+        <h3>${movie.Title}</h3>
+        <img src="${movie.Poster}" alt="${movie.Title}">
+        <button class="add-to-favorites">Add to Favorites</button>
+        <button class="view-details">View Details</button>
+    `;
+
+    const addToFavoritesBtn = movieElement.querySelector('.add-to-favorites');
+    addToFavoritesBtn.addEventListener('click', () => {
+        addToFavorites(movie, addToFavoritesBtn, isSearchResult);
+    });
+
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    if (favorites.some(favMovie => favMovie.imdbID === movie.imdbID)) {
+        addToFavoritesBtn.textContent = 'Added to Favorites';
+        addToFavoritesBtn.disabled = true;
+    }
+
+    const viewDetailsBtn = movieElement.querySelector('.view-details');
+    viewDetailsBtn.addEventListener('click', () => {
+        window.location.href = `movie-details.html?id=${movie.imdbID}`;
+    });
+
+    return movieElement;
+}
+
+// Add event listener on movie-details.html
+if (window.location.pathname.includes('/movie-details.html')) {
+    document.addEventListener('DOMContentLoaded', () => {
+        viewMovieDetails();
+    });
+}
+
+// Function to add a movie to favorites
+function addToFavorites(movie, button, isSearchResult) {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const isAlreadyAdded = favorites.some(favMovie => favMovie.imdbID === movie.imdbID);
+
+    if (!isAlreadyAdded) {
+        favorites.push(movie);
+        localStorage.setItem('favorites', JSON.stringify(favorites));
+
+        button.textContent = 'Added to Favorites';
+        button.disabled = true;
+
+        if (!isSearchResult) {
+            displayFavorites(favorites);
+        }
+    }
+}
+
+// Function to display favorites
+function displayFavorites(favorites) {
+    favoritesList.innerHTML = '';
+
+    if (favorites.length > 0) {
+        favorites.forEach(movie => {
+            const favoriteMovieElement = createFavoriteMovieElement(movie);
+            favoritesList.appendChild(favoriteMovieElement);
+        });
+    } else {
+        favoritesList.innerHTML = '<p>You have no favorite movies.</p>';
+    }
+}
+
+// Function to create a favorite movie element
+function createFavoriteMovieElement(movie) {
+    const favoriteMovieElement = document.createElement('div');
+    favoriteMovieElement.classList.add('movie');
+    favoriteMovieElement.innerHTML = `
+        <h3>${movie.Title}</h3>
+        <img src="${movie.Poster}" alt="${movie.Title}">
+        <button class="remove-from-favorites">Remove from Favorites</button>
+    `;
+
+    const removeFromFavoritesBtn = favoriteMovieElement.querySelector('.remove-from-favorites');
+    removeFromFavoritesBtn.addEventListener('click', () => {
+        removeFromFavorites(movie, favoriteMovieElement);
+    });
+
+    return favoriteMovieElement;
+}
+
+// Function to remove a movie from favorites
+function removeFromFavorites(movie, element) {
+    const favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const updatedFavorites = favorites.filter(favMovie => favMovie.imdbID !== movie.imdbID);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+
+    element.remove();
+}
+
+// Function to view movie details
+async function viewMovieDetails() {
+    const queryParams = new URLSearchParams(window.location.search);
+    const movieId = queryParams.get('id');
+
+    const response = await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movieId}`);
+    const movieDetails = await response.json();
+
+    detailsContainer.innerHTML = `
+        <h2>${movieDetails.Title}</h2>
+        <img src="${movieDetails.Poster}" alt="${movieDetails.Title}">
+        <p><strong>Year:</strong> ${movieDetails.Year}</p>
+        <p><strong>Rated:</strong> ${movieDetails.Rated}</p>
+        <p><strong>Released:</strong> ${movieDetails.Released}</p>
+        <p><strong>Runtime:</strong> ${movieDetails.Runtime}</p>
+        <p><strong>Genre:</strong> ${movieDetails.Genre}</p>
+        <p><strong>Plot:</strong> ${movieDetails.Plot}</p>
+    `;
+}
